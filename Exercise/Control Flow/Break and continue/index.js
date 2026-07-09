36d2ef4c84164c77c9a8ac68d03676260f418b56
Ridwan2024-P <ahsanrid@gmail.com>
2026-07-09 22:41:11 +0600

    break and continue:

---
+++ Exercise/Control Flow/Break and continue/index.js
@@ -0,0 +1,16 @@
+
+const person ={
+    name:'rid',
+    age:20
+}
+for(let key in person)
+    console.log(key ,person[key]);
+ 
+const color = ['black','blue',  'red ']
+    for(let i in color){
+        console.log(i,color[i]);
+    }
+
+
+    for(let colors of color)
+        console.log(colors)
--- Exercise/Control Flow/Infinite Loop/index.js
+++ Exercise/Control Flow/Infinite Loop/index.js
@@ -1,16 +1,6 @@
-
-const person ={
-    name:'rid',
-    age:20
+let i = 0;
+while(i<5){
+    // if(i === 2) break;
+    i++;
+    continue;
 }
-for(let key in person)
-    console.log(key ,person[key]);
- 
-const color = ['black','blue',  'red ']
-    for(let i in color){
-        console.log(i,color[i]);
-    }
-
-
-    for(let colors of color)
-        console.log(colors)
