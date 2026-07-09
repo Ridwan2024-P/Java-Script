b1ed73f103d45b6659e328a45d2b96a375ca1249
Ridwan2024-P <ahsanrid@gmail.com>
2026-07-09 12:35:41 +0600

    logical Operators

---
+++ Exercise/Operators/Logical Operators with Non-booleans/index.js
@@ -0,0 +1,32 @@
+// Logical AND (&&)
+// Return True if both operands are True
+console.log(true && true);
+
+let highIncome =true;
+let goodCreditScore = true;
+let eligibleForLoan = highIncome && goodCreditScore ;
+
+console.log(eligibleForLoan);
+
+
+// Logical OR (||)
+// Return True if one operands are True
+console.log(true && true);
+
+let highIncome =true;
+let goodCreditScore = true;
+let eligibleForLoan = highIncome || goodCreditScore ;
+
+console.log(eligibleForLoan);
+
+
+
+// Logical NOt (!)
+// Return True if one operands are True
+console.log(true && true);
+
+let highIncome =true;
+let goodCreditScore = true;
+let eligibleForLoan = highIncome =! goodCreditScore ;
+
+console.log(eligibleForLoan);
