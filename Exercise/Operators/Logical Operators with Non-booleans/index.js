ef81185c3b15b7f806ef1f21c4e83e45ef636b8c
Ridwan2024-P <ahsanrid@gmail.com>
2026-07-09 12:42:31 +0600

    Non Booleans

--- Exercise/Operators/Logical Operators with Non-booleans/index.js
+++ Exercise/Operators/Logical Operators with Non-booleans/index.js
@@ -11,22 +11,42 @@ console.log(eligibleForLoan);
 
 // Logical OR (||)
 // Return True if one operands are True
-console.log(true && true);
+// console.log(true && true);
 
-let highIncome =true;
-let goodCreditScore = true;
-let eligibleForLoan = highIncome || goodCreditScore ;
+// let highIncome =true;
+// let goodCreditScore = true;
+// let eligibleForLoan = highIncome || goodCreditScore ;
 
-console.log(eligibleForLoan);
+// console.log(eligibleForLoan);
 
 
 
 // Logical NOt (!)
 // Return True if one operands are True
-console.log(true && true);
+// console.log(true && true);
 
-let highIncome =true;
-let goodCreditScore = true;
-let eligibleForLoan = highIncome =! goodCreditScore ;
+// let highIncome =true;
+// let goodCreditScore = true;
+// let eligibleForLoan = highIncome =! goodCreditScore ;
 
-console.log(eligibleForLoan);
+// console.log(eligibleForLoan);
+
+// false || true --> true
+//false || 'Rid' --> Rid
+//false || 1 --> 1
+
+
+//Falsy (false)
+//Undefined
+//null
+//0
+//false
+//''
+//NaN
+
+//Anything that is not False --> Truthy
+
+let userColor = undefined;
+let defaultColor = 'blue';
+let currrentColor = userColor || defaultColor;
+console.log(currrentColor);
