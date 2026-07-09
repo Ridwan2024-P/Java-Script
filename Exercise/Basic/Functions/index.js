0a2d543b620d773934f164e9801b34c8fafe57f2
Ridwan2024-P <ahsanrid@gmail.com>
2026-07-09 12:06:35 +0600

    type of function

--- Exercise/Basic/Functions/index.js
+++ Exercise/Basic/Functions/index.js
@@ -2,9 +2,17 @@ function greet(){
     console.log('Hello World');
 }
 greet();
-
+//Performing a task
 function greet1(name,lastName){
     console.log('Hello World ' + name  + lastName);
 }
+
 greet1('Rid');
 greet1('Ridwan','Bin Ahsan' );
+
+//calculating a value
+function square(number){
+    return number * number;
+}
+let number = square(2);
+console.log(number);
