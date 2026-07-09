af10a15ce69b576bd21cc6cb9cb1eb1a9f15c299
Ridwan2024-P <ahsanrid@gmail.com>
2026-07-09 22:13:28 +0600

    Switch

---
+++ Exercise/Control Flow/Switch/index.js
@@ -0,0 +1,12 @@
+let role = 'Guest';
+
+switch(role){
+    case 'Guest':
+    console.log("Guest User");
+    break;
+    case 'admin':
+        console.log("Admin User");
+    break;
+    default:
+        console.log("Unknow User");
+}
