41fe6731fc87c813eee3b1ac3cc0d3a7d6c4f04c
Ridwan2024-P <ahsanrid@gmail.com>
2026-07-09 12:29:54 +0600

    Ternary Operators

---
+++ Exercise/Operators/Ternary Operators/index.js
@@ -0,0 +1,7 @@
+//if a customer has more than 100 points
+//they are a 'gold' customer,otherwise,
+//they are a 'silver' customer
+
+let points = 110;
+let type = points > 120 ? 'gold' : 'silver';
+console.log(type)
