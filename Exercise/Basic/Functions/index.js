26fe61723a49e9e536120121dc711e2f803a5825
Ridwan2024-P <ahsanrid@gmail.com>
2026-07-09 12:01:40 +0600

    Function

---
+++ Exercise/Basic/Functions/index.js
@@ -0,0 +1,10 @@
+function greet(){
+    console.log('Hello World');
+}
+greet();
+
+function greet1(name,lastName){
+    console.log('Hello World ' + name  + lastName);
+}
+greet1('Rid');
+greet1('Ridwan','Bin Ahsan' );
