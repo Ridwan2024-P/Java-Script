e010d45b4acccd1455538b4b97d43e50f6677c7b
Ridwan2024-P <ahsanrid@gmail.com>
2026-07-10 12:41:34 +0600

    rest and let var and local and golobal

--- Exercise/Function/index.js
+++ Exercise/Function/index.js
@@ -4,3 +4,10 @@ function sum(a,b){
     return a + b;
 }
 console.log (sum(1,2,3,4,5));
+
+// rest oeratoe
+function sum(...args){
+    console.log(arguments)
+    return a + b;
+}
+console.log (sum(1,2,3,4,5));
