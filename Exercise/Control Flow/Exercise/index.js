d53eb5fab79404bb8397a11c2a578fb6aa08af90
Ridwan2024-P <ahsanrid@gmail.com>
2026-07-10 11:18:10 +0600

    demerit point

--- Exercise/Control Flow/Exercise/index.js
+++ Exercise/Control Flow/Exercise/index.js
@@ -3,3 +3,21 @@ function isLandscape(width,hight){
 return width>hight;
 }
 console.log(isLandscape(300>600));
+
+
+
+
+function checkSpeed (speed){
+    const speedLimit = 70;
+    const KmPerPoint = 5;
+    if(speed < speedLimit)
+        console.log('OK');
+    else{
+        let points = Math.floor((speed - speedLimit) /KmPerPoint);
+        if (points >= 12)
+            console.log('License suspended');
+        else
+            console.log('Points' ,points);
+    }
+}
+checkSpeed(50);
