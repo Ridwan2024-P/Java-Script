b227a358c324da4fac88649b25df9d8574813914
Ridwan2024-P <ahsanrid@gmail.com>
2026-07-10 12:23:27 +0600

    finding array

--- Exercise/Array/Adding Array/index.js
+++ Exercise/Array/Adding Array/index.js
@@ -2,3 +2,10 @@
  numbers.push(5,6);
   numbers.unshift(1,2);
  console.log(numbers)
+
+//  Finding array
+
+const array = [1,2,3,4,5,1];
+console.log(array.indexOf(1,2));
+console.log(array.lastIndexOf(1));
+console.log(array.includes(1));
