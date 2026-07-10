cd374100117a3b50d78e02437016f457fa5991f2
Ridwan2024-P <ahsanrid@gmail.com>
2026-07-10 12:15:58 +0600

    equal object

--- Exercise/Object/Exercise/index.js
+++ Exercise/Object/Exercise/index.js
@@ -1,10 +1,22 @@
-let address = {
-    street: 'a',
-    city:'b',
-    zipCode:c
-};
-function showAddress(address){
-    for(let key in address){
-        console.log(key,address[key]);
-    }
+// let address = {
+//     street: 'a',
+//     city:'b',
+//     zipCode:c
+// };
+// function showAddress(address){
+//     for(let key in address){
+//         console.log(key,address[key]);
+//     }
+// }
+
+function address (a , b , c){
+    this.a=a;
+    this.b=b;
+    this.c=c;
+}
+let address1 =new address ('a','b','c');
+let address2 =new address ('a','b','c');
+function areEqual(address1,address2){
+    if(address1 ===  address2) console.log("Equal");
+    else console.log("not Equal");
 }
