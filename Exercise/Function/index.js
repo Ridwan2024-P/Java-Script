5782bca3aa695fd84e89debb7fe79069f140f543
Ridwan2024-P <ahsanrid@gmail.com>
2026-07-10 12:47:24 +0600

    Sum Arguments and area and error

--- Exercise/Function/index.js
+++ Exercise/Function/index.js
@@ -11,3 +11,21 @@ function sum(...args){
     return a + b;
 }
 console.log (sum(1,2,3,4,5));
+
+
+// Exercise
+function sum(...args) {
+    return args.reduce((a, b) => a + b, 0);
+}
+
+console.log(sum(1, 2, 3, 4, 5));
+
+const circle = {
+    radius: 1,
+
+    get area() {
+        return Math.PI * this.radius * this.radius;
+    }
+};
+
+console.log(circle.area);
