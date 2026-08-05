be7fff848473ca7260c32ff432bedb0ffe2bbebb
Ridwan2024-P <ahsanrid@gmail.com>
2026-08-05 12:39:23 +0600

    loader add

--- Project/Quote Generator/Scrpit.js
+++ Project/Quote Generator/Scrpit.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+// Show loadering
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hidding Loading
+ function complate(){
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+ }
 
 // Get Quotes From API
 let apiQuotes = [];
 function newQuote() {
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+     loading();
     // authorText.textContent = quote.author;
     //check if Author field is blank and replace it with
     if(!quote.author){
@@ -19,20 +33,24 @@ function newQuote() {
     }
     if(quote.quote.length > 50){
         quoteText.classList.add('long-quote');
+        
     }
     else{
         quote.text.classList.remove('long-quote')
     }
     quoteText.textContent = quote.quote;
+    complate();
 }
 
 async function getQuotes() {
     const apiUrl = "https://dummyjson.com/quotes";
+   
     try {
         const response = await fetch(apiUrl);
         const data = await response.json();
         apiQuotes = data.quotes;
         newQuote();
+       
     } catch (error) {
         console.error(error);
     }
