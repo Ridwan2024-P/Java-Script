0c8d0ab32323d1f4675e4496236845712a56be14
Ridwan2024-P <ahsanrid@gmail.com>
2026-08-17 16:00:34 +0600

    video play pause done

--- Project/video Player/script.js
+++ Project/video Player/script.js
@@ -1,8 +1,28 @@
-
+const video = document.querySelector('video');
+const ProgressRange = document.querySelector('.progress-range')
+const progressBar = document.querySelector('.Progress-bar');
+const playBtn = document.getElementById('play-btn');
+const volumeIcon = document.getElementById('volume-icon');
+const volumeRange = document.querySelector('.volume-range');
+const volumeBar = document.querySelector('.volume-bar');
+const currentTime = document.querySelector('.time-elapsed');
+const duration = document.querySelector('.time-duration');
+const FullscreenBtn = document.querySelector('.fullscreen');
 
 // Play & Pause ----------------------------------- //
 
-
+function togglePlay(){
+    if(video.paused){
+        video.play();
+        playBtn.classList.replace('fa-play', 'fa-pause');
+        playBtn.setAttribute('title','Pause');
+    }
+    else{
+        video.pause();
+        playBtn.classList.replace('fa-pause', 'fa-play');
+        playBtn.setAttribute('title','Play');
+    }
+}
 
 // Progress Bar ---------------------------------- //
 
@@ -19,3 +39,5 @@
 // Fullscreen ------------------------------- //
 
 
+playBtn.addEventListener('click' , togglePlay);
+video.addEventListener('click',togglePlay);
